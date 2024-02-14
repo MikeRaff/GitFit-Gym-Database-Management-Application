bd5e6940acc23274c7739a54bc8b987e2adceddf
Erik Cupsa <2003eec@gmail.com>
2024-02-14 17:02:15 -0500

    Initial spring boot set up

---
+++ ClassRegistration/src/test/java/ca/mgcill/ecse321/ClassRegistration/ClassRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mgcill.ecse321.ClassRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
