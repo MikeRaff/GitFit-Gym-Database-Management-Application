a1e6528d7d884e69f19f82de35355f6f96b17109
Erik Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2024-02-15 14:32:56 -0500

    Initial Spring files

---
+++ gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/GymregistrationApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.gymregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymregistrationApplication.class, args);
+	}
+
+}
