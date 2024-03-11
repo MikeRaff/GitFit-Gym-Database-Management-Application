bc9ff2654569f3ddd19353c0c3f7cb6193a70b98
Erik Cupsa <2003eec@gmail.com>
2024-03-11 14:41:22 -0400

    Changed CustomerTests methods to be under 20 lines

--- gymregistration/src/test/java/ca/mcgill/ecse321/gymregistration/repository/CustomerTests.java
+++ gymregistration/src/test/java/ca/mcgill/ecse321/gymregistration/repository/CustomerTests.java
@@ -29,77 +29,60 @@ public class CustomerTests {
         customerRepository.deleteAll();
         personRepository.deleteAll();
     }
-
     @Test
     public void testCreateAndReadCustomer() {
         // Create and persist person.
-        String name = "Jim";
-        Person jim = new Person(name);
-        jim = personRepository.save(jim);
-        
+        Person jim = createAndPersistPerson("Jim");
+
         // Create customer.
-        String email = "customeremail@emailprovider.net";
-        String password = "password";
-        int creditCardNumber = 123456789;
-        Customer customer = new Customer(email, password, jim, creditCardNumber);
-    
-        // Save customer to database.
-        customer = customerRepository.save(customer);
-    
-        // Read customer from database.
+        Customer customer = createAndPersistCustomer("customeremail@emailprovider.net", "password", jim, 123456789);
+
+        // Read customer from the database.
         Customer customerFromDB = customerRepository.findCustomerById(customer.getId());
 
-        // Assert customer is not null and has correct non-null attributes.
-        assertNotNull(customerFromDB);
-        
-        assertEquals(customer.getId(), customerFromDB.getId());
-        assertEquals(email, customerFromDB.getEmail());
-        assertEquals(password, customerFromDB.getPassword());
-        assertEquals(creditCardNumber, customerFromDB.getCreditCardNumber());
-        
-        assertNotNull(customerFromDB.getPerson());
-        //assertEquals(jim, customerFromDB.getPerson());    // compares addresses, not values
-
-        // Assert person is not null and has correct attributes.
-        assertNotNull(personRepository.findPersonById(jim.getId()));
-        
-        assertEquals(jim.getId(), customerFromDB.getPerson().getId());
-        assertEquals(name, customerFromDB.getPerson().getName());
+        // Assertions
+        assertCustomerAttributes(customer, customerFromDB);
+        assertPersonAttributes(jim, customerFromDB.getPerson());
     }
+
     @Test
     public void testFindCustomersByPersonName() {
         // Create and persist person.
-        String name = "Bob Johnson";
-        Person bob = new Person(name);
-        bob = personRepository.save(bob);
+        Person bob = createAndPersistPerson("Bob Johnson");
 
         // Create customers with the same person.
-        String email1 = "customer1@emailprovider.ca";
-        String password1 = "customer1Password";
-        int creditCardNumber1 = 987654321;
-        Customer customer1 = new Customer(email1, password1, bob, creditCardNumber1);
-        customerRepository.save(customer1);
-
-        String email2 = "customer2@emailprovider.ca";
-        String password2 = "customer2Password";
-        int creditCardNumber2 = 456789123;
-        Customer customer2 = new Customer(email2, password2, bob, creditCardNumber2);
-        customerRepository.save(customer2);
+        createAndPersistCustomer("customer1@emailprovider.ca", "customer1Password", bob, 987654321);
+        createAndPersistCustomer("customer2@emailprovider.ca", "customer2Password", bob, 456789123);
 
         // Find customers by person name.
-        List<Customer> customers = customerRepository.findCustomersByPerson_Name(name);
+        List<Customer> customers = customerRepository.findCustomersByPerson_Name(bob.getName());
 
-        // Assert the list is not null and contains the expected number of customers.
+        // Assertions
         assertNotNull(customers);
         assertEquals(2, customers.size());
+    }
 
-        // Assert that the customers in the list have the correct attributes.
-        assertEquals(email1, customers.get(0).getEmail());
-        assertEquals(password1, customers.get(0).getPassword());
-        assertEquals(creditCardNumber1, customers.get(0).getCreditCardNumber());
+    private Person createAndPersistPerson(String name) {
+        Person person = new Person(name);
+        return personRepository.save(person);
+    }
 
-        assertEquals(email2, customers.get(1).getEmail());
-        assertEquals(password2, customers.get(1).getPassword());
-        assertEquals(creditCardNumber2, customers.get(1).getCreditCardNumber());
+    private Customer createAndPersistCustomer(String email, String password, Person person, int creditCardNumber) {
+        Customer customer = new Customer(email, password, person, creditCardNumber);
+        return customerRepository.save(customer);
+    }
+
+    private void assertCustomerAttributes(Customer expected, Customer actual) {
+        assertNotNull(actual);
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getEmail(), actual.getEmail());
+        assertEquals(expected.getPassword(), actual.getPassword());
+        assertEquals(expected.getCreditCardNumber(), actual.getCreditCardNumber());
+    }
+
+    private void assertPersonAttributes(Person expected, Person actual) {
+        assertNotNull(actual);
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getName(), actual.getName());
     }
 }
