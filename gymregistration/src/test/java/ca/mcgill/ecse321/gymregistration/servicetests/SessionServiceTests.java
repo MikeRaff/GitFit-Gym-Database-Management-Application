59938a29186e92a5338e1ba05459cf4929119547
WilliamZimmerman <William.zimmerman@mail.mcgill.ca>
2024-03-20 13:03:09 -0400

    Added test for session

--- gymregistration/src/test/java/ca/mcgill/ecse321/gymregistration/servicetests/SessionServiceTests.java
+++ gymregistration/src/test/java/ca/mcgill/ecse321/gymregistration/servicetests/SessionServiceTests.java
@@ -13,14 +13,39 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import ca.mcgill.ecse321.gymregistration.service;
-@SpringBootTest
-public class SessionServiceTests {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-    SessionService sessionservice = new SessionService();
+import ca.mcgill.ecse321.gymregistration.model.ClassType;
+import ca.mcgill.ecse321.gymregistration.model.Session;
+import ca.mcgill.ecse321.gymregistration.service.SessionService;
+@SpringBootTest
+public class SessionServiceTests { 
+    @Test
     public void testCreateSession()
     {
 
+    ClassType classType = new ClassType("goat yoga", true);
+    Session expected = new Session(("2024-3-20", "8:00", "9:15","A description", "sessionName","room 1", classType);
+    SessionService sessionService = new SessionService();
+    Session session = sessionService.createSession("2024-3-20", "8:00", "9:15","A description", "sessionName","room 1", classType);
+    
+    assertSessionAttributes(expected, session);
+    
+    }
+
+    private void assertSessionAttributes(Session expected, Session actual) {
+        assertNotNull(actual);
+        assertEquals(expected.getId(), actual.getId());
+        assertEquals(expected.getDate(), actual.getDate());
+        assertEquals(expected.getStartTime(), actual.getStartTime());
+        assertEquals(expected.getEndTime(), actual.getEndTime());
+        assertEquals(expected.getDescription(), actual.getDescription());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getLocation(), actual.getLocation());
     }
 }
 
