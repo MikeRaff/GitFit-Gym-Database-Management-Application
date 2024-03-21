16572c8474ffd1545d719db952e9871882d40a2c
Erik Cupsa <2003eec@gmail.com>
2024-03-21 17:32:38 -0400

    removed all broken files

--- gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/dao/InstructorRegistrationRepository.java
+++ gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/dao/InstructorRegistrationRepository.java
@@ -9,8 +9,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface InstructorRegistrationRepository extends CrudRepository<InstructorRegistration, Integer> {
     InstructorRegistration findInstructorRegistrationById(int id);
-    InstructorRegistration findInstructorRegistrationByInstructorAndSession(Instructor instructor, Session session);
-    List<InstructorRegistration> findInstructorRegistrationsBySession(Session session);
-    InstructorRegistration findInstructorRegistrationByInstructorAndSession(Instructor instructor, Session session);
-    List<InstructorRegistration> findInstructorRegistrationsBySession(Session session);
 }
