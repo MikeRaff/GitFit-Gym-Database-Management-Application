2f80952fb9f8e635a363181be4bd1bf3da293779
Billy <30451590+WilliamZimmerman@users.noreply.github.com>
2024-03-21 16:29:36 -0400

    Update InstructorRegistrationRepository.java
    add imports to support functionality

--- gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/dao/InstructorRegistrationRepository.java
+++ gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/dao/InstructorRegistrationRepository.java
@@ -1,8 +1,15 @@
 package ca.mcgill.ecse321.gymregistration.dao;
 
+import java.util.List;
+
+import ca.mcgill.ecse321.gymregistration.model.Instructor;
 import ca.mcgill.ecse321.gymregistration.model.InstructorRegistration;
+import ca.mcgill.ecse321.gymregistration.model.Session;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface InstructorRegistrationRepository extends CrudRepository<InstructorRegistration, Integer> {
     InstructorRegistration findInstructorRegistrationById(int id);
+    InstructorRegistration findInstructorRegistrationByInstructorAndSession(Instructor instructor, Session session);
+    List<InstructorRegistration> findInstructorRegistrationsBySession(Session session);
 }
