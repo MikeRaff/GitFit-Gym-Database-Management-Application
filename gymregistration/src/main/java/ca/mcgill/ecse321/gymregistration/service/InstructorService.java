dde4b75ad47467b20eb35d1fb2dad43972b83f0c
WilliamZimmerman <William.zimmerman@mail.mcgill.ca>
2024-03-21 16:59:36 -0400

    Changed InstructorService and InstructorRegistrationService

--- gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/service/InstructorService.java
+++ gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/service/InstructorService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import ca.mcgill.ecse321.gymregistration.dao.InstructorRegistrationRepository;
 import ca.mcgill.ecse321.gymregistration.dao.InstructorRepository;
 import ca.mcgill.ecse321.gymregistration.dao.SessionRepository;
+import ca.mcgill.ecse321.gymregistration.model.ClassType;
 import ca.mcgill.ecse321.gymregistration.model.Instructor;
 import ca.mcgill.ecse321.gymregistration.model.InstructorRegistration;
 import ca.mcgill.ecse321.gymregistration.model.Session;
@@ -17,50 +18,15 @@ import jakarta.transaction.Transactional;
 public class InstructorService {
     @Autowired
     InstructorRepository instructorRepository;
-    @Autowired
-    SessionRepository sessionRepository;
-    @Autowired
-    InstructorRegistrationRepository instructorRegistrationRepository;
-    /**
-     * Create a new instructor registeration to add an instructor to a class
-     * @param session
-     * @param email
-     * @return new registration
-     * @throws GRSException Invalid email or already registered
-     */
-    public  InstructorRegistration registerInstructorForClass(Session session, String email)
-    {
-        Instructor instructor = instructorRepository.findInstructorByEmail(email);
-        if (instructor == null) {
-            throw new GRSException(HttpStatus.UNAUTHORIZED, "Invalid email");
-        }
-        InstructorRegistration instructorRegistration = instructorRegistrationRepository.findInstructorRegistrationByInstructorAndSession(instructor, session);
-        if(instructorRegistration !=null)
-        {
-            throw new GRSException(HttpStatus.UNAUTHORIZED, "already registered");
-        }
-        instructorRegistration = new InstructorRegistration(session.getDate(),instructor, session);
-        instructorRegistrationRepository.save(instructorRegistration);
-        return instructorRegistration;
-    }
-    /**
-     * Remove an instructor from a class if there is another class
-     * @param Session
-     * @param email
-     * @throws GRSException not able to remove instructor
-     */
+    
+
     @Transactional
-    public void removeInstructorFromClass(Session Session, String email)
+    public List<Instructor> getAllInstructors()
     {
-        List<InstructorRegistration> instructorRegistrations = instructorRegistrationRepository.findInstructorRegistrationsBySession(Session);
-        if(instructorRegistrations.size() <2)
-            throw new GRSException(HttpStatus.UNAUTHORIZED, "not enough instructors registered");
-        for(InstructorRegistration r: instructorRegistrations )
-        {
-            if(r.getInstructor().getEmail().equals(email))
-                instructorRegistrationRepository.delete(r);
-            return;
+         List<Instructor> instructors = instructorRepository.findAll();
+        if(instructors.size() == 0){
+            throw new GRSException(HttpStatus.NOT_FOUND, "No Instructors found in the system.");
         }
-        throw new GRSException(HttpStatus.UNAUTHORIZED, "instructor not teaching course");
+        return instructors;
     }
 }
