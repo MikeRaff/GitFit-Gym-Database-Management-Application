5fac1e3733c18b1bcf08bb56d3d9b29ced75dee2
Erik Cupsa <2003eec@gmail.com>
2024-03-24 20:59:40 -0400

    Fixed customer registration controller and service

--- gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/service/CustomerRegistrationService.java
+++ gymregistration/src/main/java/ca/mcgill/ecse321/gymregistration/service/CustomerRegistrationService.java
@@ -42,12 +42,10 @@ public class CustomerRegistrationService {
      * @throws GRSException if attempt to register customer to session is invalid
      */
     @Transactional
-    public CustomerRegistration registerCustomerToSession(int sessionId, String email){
-
+    public CustomerRegistration registerCustomerToSession(int sessionId, String email){ //note: method can not be reduced to 20 lines do to multiple error checks
         if (sessionId == 0 || email == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "No session or customer entered.");
         }
-
         Customer customer = customerRepository.findCustomerByEmail(email);
         if (customer == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "Customer not found.");
@@ -55,33 +53,25 @@ public class CustomerRegistrationService {
         if (customer.getCreditCardNumber() == 0){
             throw new GRSException(HttpStatus.UNAUTHORIZED, "Credit card must be entered to register for a class.");
         }
-        
         Session session = sessionRepository.findSessionById(sessionId);
         if(session == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "Session not found.");
         }
-
         CustomerRegistration customerRegistration = customerRegistrationRepository.findCustomerRegistrationByCustomerAndSession(customer, session);
         if (customerRegistration != null){
             throw new GRSException(HttpStatus.CONFLICT, "Cannot register for same session twice.");
         }
-        
         List<CustomerRegistration> customerRegistrations = customerRegistrationRepository.findCustomerRegistrationsBySession_Id(sessionId);
         if(customerRegistrations.size() == session.getCapacity()){
             throw new GRSException(HttpStatus.UNAUTHORIZED, "Session is already at capacity.");
         }
-
-        //getting current date and time
+        //getting current date and time, converting startTime and date to LocalDateTime, and calculating difference
         LocalDateTime currentDateTime = LocalDateTime.now();
-        //converting startTime and date to LocalDateTime
         LocalDateTime sessionDatetime = LocalDateTime.of(session.getDate().toLocalDate(), session.getStartTime().toLocalTime());
-        //calculating difference
         Duration timeDifference = Duration.between(currentDateTime, sessionDatetime);
-        //checking if registration time (current) is before the start of the session
-        if (timeDifference.toHours() < 0){
+        if (timeDifference.toHours() < 0){         //checking if registration time (current) is before the start of the session
             throw new GRSException(HttpStatus.BAD_REQUEST, "Cannot register for in-progress or completed session.");
         }
-
         customerRegistration = new CustomerRegistration(session.getDate(), session, customer);     
         customerRegistrationRepository.save(customerRegistration);
         return customerRegistration;
@@ -112,6 +102,35 @@ public class CustomerRegistrationService {
             throw new GRSException(HttpStatus.NOT_FOUND, "The registration does not exist.");
         }
         return customerRegistration;
+    }
+
+    /**
+     * UpdateCustomerRegistration: updating a customer registration with new session
+     * @param oldSessionId: old session
+     * @param newSessionId: new session
+     * @param email: email of customer
+     * @return Updated customer registration
+     */
+    @Transactional
+    public CustomerRegistration updateCustomerRegistration(int oldSessionId, int newSessionId, String email) {
+        if (email == null){
+            throw new GRSException(HttpStatus.BAD_REQUEST, "No session or customer entered.");
+        }
+        Customer customer = customerRepository.findCustomerByEmail(email);
+        if (customer == null){
+            throw new GRSException(HttpStatus.NOT_FOUND, "Customer not found.");
+        }
+        Session toUpdate = sessionRepository.findSessionById(oldSessionId);
+        if(toUpdate == null){
+            throw new GRSException(HttpStatus.NOT_FOUND, "Old session not found.");
+        }
+        Session session = sessionRepository.findSessionById(newSessionId);
+        CustomerRegistration customerRegistration = customerRegistrationRepository.findCustomerRegistrationByCustomerAndSession(customer, toUpdate);
+        if (customerRegistration == null){
+            throw new GRSException(HttpStatus.NOT_FOUND, "The registration does not exist.");
+        }
+        CustomerRegistration updated = new CustomerRegistration(session.getDate(), session, customerRepository.findCustomerByEmail(email));
+        return customerRegistrationRepository.save(updated);
     }
 
     /**
@@ -168,38 +187,32 @@ public class CustomerRegistrationService {
      * @throws GRSException if attempt to remove customer from session is invalid
      */
     @Transactional
-    public void removeCustomerFromSession(int sessionId, String email, GymUser gymUser) {
+    public void removeCustomerFromSession(int sessionId, String email, GymUser gymUser) { //note: can not reduce to 20 lines do to multiple error checks
         if (sessionId == 0 || email == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "No session or customer entered.");
         }
-        if((gymUser instanceof Customer && !gymUser.getEmail().equals(email)) || (gymUser instanceof Instructor && instructorRegistrationRepository.findInstructorRegistrationByInstructor_idAndSession_id(gymUser.getId().intValue(), sessionId) == null)){
+        if((gymUser instanceof Customer && !gymUser.getEmail().equals(email)) || (gymUser instanceof Instructor && instructorRegistrationRepository.findInstructorRegistrationByInstructor_idAndSession_id(gymUser.getId(), sessionId) == null)){
             throw new GRSException(HttpStatus.UNAUTHORIZED, "Customers can only be removed from sessions by the owner, instructor or themselves.");
         }
         Customer customer = customerRepository.findCustomerByEmail(email);
         Session session = sessionRepository.findSessionById(sessionId);
-
         if (customer == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "Customer not found.");
         } 
         if(session == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "Session not found.");
         }
-
         CustomerRegistration customerRegistration = customerRegistrationRepository.findCustomerRegistrationByCustomerAndSession(customer, session);
         if (customerRegistration == null){
             throw new GRSException(HttpStatus.NOT_FOUND, "The registration does not exist.");
         }
-
-        // current date and time
+        // current and session date and time
         LocalDateTime currentDateTime = LocalDateTime.now();
-        // session date and time
         LocalDateTime sessionDateTime = LocalDateTime.of(session.getDate().toLocalDate(), session.getStartTime().toLocalTime());
-        // getting difference
-        Duration timeDifference = Duration.between(currentDateTime, sessionDateTime);
+        Duration timeDifference = Duration.between(currentDateTime, sessionDateTime);// getting difference
         if (timeDifference.toHours() < 48){
             throw new GRSException(HttpStatus.BAD_REQUEST, "Cancellation of registration must be at least 48 hours before session start.");
         }
-
         customerRegistrationRepository.delete(customerRegistration);
     }
 
