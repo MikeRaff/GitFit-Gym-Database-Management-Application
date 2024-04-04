821e1cc30e8b07e3e777419713098d45d4aa6a6c
Erik Cupsa <2003eec@gmail.com>
2024-04-04 19:12:03 -0400

    Some work on Home page

---
+++ GymRegistration-Frontend/src/components/Logo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="logo-container">
+      <img class="solid-logo animate" src="@/assets/logo.png" alt="Student Studying" />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Logo'
+  }
+  </script>
+  
+  <style scoped>
+.logo-container {
+    z-index: 0;
+    width: 500px;
+    height: 500px;
+    opacity: 1;
+    position: absolute;
+    top: 0;
+    right: 15%;
+    bottom: 0;
+    left: 50%;
+    margin: auto;
+    animation: fadeIn 2s linear both; 
+    animation-delay: 1.4s; 
+    &.animate {
+      opacity: 1;
+    }
+  
+    .solid-logo {
+      position: absolute;
+      top: auto;
+      right: auto;
+      bottom: auto;
+      left: 0;
+      margin: auto;
+      width: 100%;
+      opacity: 0;
+      z-index: 1;
+  
+      &.animate {
+        opacity: 1;
+      }
+    }
+  }
+  </style>
